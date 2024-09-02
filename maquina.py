6dc56f7756764b4e826fae253ab83c6edbf8e5dd
Miria-Kelly <160160742+Miria-Kelly@users.noreply.github.com>
2024-09-01 21:29:12 -0300

    Add files via upload

---
+++ maquina.py
@@ -0,0 +1,33 @@
+import leitor, moinho, armazen, impressora
+resultado = []
+codigo, posicao, numeros = (leitor.ler_cartao("cartao.in"))
+for c in range(0,len(codigo)):
+    if codigo[c] == "0001":
+        memoria = armazen.armazenar(numeros[c], posicao[c])
+
+    elif codigo[c] == "0010":
+        moinho.carregar(posicao[c])
+
+    elif codigo[c] == "0011":
+        soma = moinho.somar()
+        resultado.append(soma)
+    elif codigo[c] == '0100':
+        subtracao = moinho.subtrair()
+        resultado.append(subtracao)
+    elif codigo[c] == "0101":
+        multi = moinho.multiplicar()
+        resultado.append(multi)
+
+    elif codigo[c] == "0110":
+        moinho.armazenar_resultado(resultado[0], posicao[c])
+
+    elif codigo[c] == "0111":
+        valor = armazen.carregar(posicao[c])
+        valor = bin(valor)[2:]
+        valor = str(valor)
+
+        impressora.escrever(valor)
+
+impressora.cartao.close()
+
+
---
+++ moinho.py
@@ -0,0 +1,55 @@
+import armazen, impressora
+valores_carregados = []
+valor = 0
+#def decifrar(codigo, posicao, numero):
+def carregar(posicao):
+    num = armazen.carregar(posicao)
+    valores_carregados.append(num)
+
+    return valores_carregados
+
+def somar():
+    resultado = valores_carregados[0] + valores_carregados[1]
+    return resultado
+
+def subtrair ():
+    resultado = (valores_carregados[0] - valores_carregados[1])
+    return resultado
+
+def multiplicar():
+    resultado = valores_carregados[0] * valores_carregados[1]
+    return resultado
+
+def armazenar_resultado(resultado, posicao):
+    return armazen.armazenar(resultado, posicao)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""def LOADOP(valor, posicao):
+    #carrega valor na posicao da memoria fornecida
+
+def ADD(a, b):
+    #soma dois operadnos que foram carregados no moinho
+
+def SUB(a, b):
+    #subtrai os dois valores que foram carregados no moinho
+
+def MUL(a, b):
+    #multiplica  os dois valores que forams careegados no moinho
+
+def STORE(resultado):
+    #armazena o resultado ultima operaçao realizada no moinho
+
+def PRINT(valor):
+    #escreve o conteudo da posicao dada do armazem"""
