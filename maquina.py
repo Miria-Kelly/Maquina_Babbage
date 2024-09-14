b3044c16006daa4255e04acbb00dd135f0cb1c9f
Heloisa Albuquerque <heloisaalbuquerque1706@gmail.com>
2024-09-14 17:08:34 -0300

    babbage

--- maquina.py
+++ maquina.py
@@ -1,37 +1,49 @@
 import leitor, moinho, armazen, impressora
 
-resultado = []
+arquivo_entrada = "cartao.in"
+arquivo_saida = "cartao.out"
 
-codigo, posicao, numeros = (leitor.ler_cartao("cartao.in"))
+def configurar(nome_arquivo_entrada = 'cartao.in', nome_arquivo_saida = 'cartao.out'):
+    global arquivo_entrada, arquivo_saida
+    arquivo_entrada = nome_arquivo_entrada
+    arquivo_saida = nome_arquivo_saida
+    #print(f'ok.{arquivo_entrada}')
+    #print(f'ok. {arquivo_saida}')
 
-for c in range(0,len(codigo)):
-    if codigo[c] == "0001":
-        memoria = armazen.armazenar(numeros[c], posicao[c])
 
-    elif codigo[c] == "0010":
-        moinho.carregar(posicao[c])
+def ligar_maquina():
 
-    elif codigo[c] == "0011":
-        soma = moinho.somar()
-        resultado.append(soma)
-    elif codigo[c] == '0100':
-        subtracao = moinho.subtrair()
-        resultado.append(subtracao)
-    elif codigo[c] == "0101":
-        multi = moinho.multiplicar()
-        resultado.append(multi)
+    codigo, posicao, numeros = leitor.ler_cartao(arquivo_entrada)
 
-    elif codigo[c] == "0110":
-        moinho.armazenar_resultado(posicao[c])
+    resultado = []
 
-    elif codigo[c] == "0111":
-        valor = armazen.carregar(posicao[c])
-        valor = bin(valor)[2:]
-        valor = str(valor)
+    for c in range(0,len(codigo)):
+        if codigo[c] == "0001":
+            memoria = armazen.armazenar(numeros[c], posicao[c])
 
-        impressora.escrever(valor)
+        elif codigo[c] == "0010":
+            moinho.carregar(posicao[c])
+
+        elif codigo[c] == "0011":
+            soma = moinho.somar()
+            resultado.append(soma)
+        elif codigo[c] == '0100':
+            subtracao = moinho.subtrair()
+            resultado.append(subtracao)
+        elif codigo[c] == "0101":
+            multi = moinho.multiplicar()
+            resultado.append(multi)
+
+        elif codigo[c] == "0110":
+            moinho.armazenar_resultado(posicao[c])
+
+        elif codigo[c] == "0111":
+            valor = armazen.carregar(posicao[c])
+            valor = bin(valor)[2:]
+            valor = str(valor)
+
+            impressora.escrever(valor, arquivo_saida)
         
-#fechando cartao.out so quando o programa finalizar por completo
-impressora.cartao.close()
-
+configurar("cartao.in", "cartao.out")
+ligar_maquina()
 
